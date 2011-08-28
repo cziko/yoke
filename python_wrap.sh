783fa60eb07ee6a2f9145008122227cfeaa8eed6
Philip Mateescu <github@philipm.us>
2011-08-28 10:22:15 -0500

    proper indentation for multi-line code

--- python_wrap.sh
+++ python_wrap.sh
@@ -1,12 +1,12 @@
-mfoo=$(echo -ne "$*" | sed '/^/ s//  /g')
+indented_py=$(echo -ne "$*" | sed '/^/ s//  /g')
 py=$( cat <<PY
 try:
-$mfoo
+$indented_py
 except Exception as e: print('Error: %s' % e)
 PY
 )
-echo "$py" > test.py
-echo "$py" | python 2>&1
+#echo "$py" > test.py
+#echo "$py" | python 2>&1
 
 python -c "$py" 2>&1
 
